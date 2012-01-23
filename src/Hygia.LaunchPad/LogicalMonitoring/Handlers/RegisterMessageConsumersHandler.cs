e0ff8ef54e93ed0df954394d440c936480bb5627
andreasohlund <andreasohlund2@gmail.com>
2012-01-23 13:13:12 +0100

    Message types now track the AC's that is processing them

---
+++ src/Hygia.LaunchPad/LogicalMonitoring/Handlers/RegisterMessageConsumersHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Hygia.LaunchPad.LogicalMonitoring.Handlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Commands;
+    using NServiceBus;
+    using Raven.Client;
+
+    public class RegisterMessageConsumersHandler : IHandleMessages<RegisterMessageConsumers>
+    {
+        readonly IDocumentSession session;
+
+        public RegisterMessageConsumersHandler(IDocumentSession session)
+        {
+            this.session = session;
+        }
+
+
+        public void Handle(RegisterMessageConsumers message)
+        {
+            var messageTypeId = message.MessageTypeId.ToString();
+
+            var messageType = session.Load<MessageType>(messageTypeId) ?? new MessageType{ Id = messageTypeId,};
+
+            if (messageType.ConsumedBy == null)
+                messageType.ConsumedBy = new List<Guid>();
+
+            messageType.ConsumedBy = messageType.ConsumedBy.Union(message.ConsumedBy).ToList();
+
+            session.Store(messageType);
+        }
+    }
+}
