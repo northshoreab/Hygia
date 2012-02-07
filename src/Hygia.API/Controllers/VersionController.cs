8a08f23c5ad0a4c1b088958dda0589d0ef08cda4
andreasohlund <andreasohlund2@gmail.com>
2012-02-07 13:02:29 +0100

    Added a version controller that outputs the SemVer version of the api at /version

---
+++ src/Hygia.API/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿namespace Hygia.API.Controllers
+{
+    using System.Diagnostics;
+    using System.Reflection;
+
+    public class VersionController
+    {
+        public string get_Version()
+        {
+            var version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+
+            return string.Format("{0}.{1}.{2}", version.FileMajorPart, version.FileMinorPart, version.FileBuildPart);
+        }
+
+    }
+}
