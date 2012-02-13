fdd69b16a74d631b9ba691e6adc8f3df8b584c41
Daniel Kjellqvist <daniel@kjellqvist.nu>
2012-02-13 08:43:16 +0100

    Added raven bootstrapper and behavior for ravensession

--- src/Hygia.API/ConfigureFubuMVC.cs
+++ src/Hygia.API/ConfigureFubuMVC.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using FubuMVC.Core;
 using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Runtime;
 using Hygia.API.Models;
+using NServiceBus;
 using Raven.Client;
 using Raven.Client.Document;
-using StructureMap;
 using StructureMap.Graph;
 
 namespace Hygia.API
@@ -26,8 +28,10 @@ namespace Hygia.API
                .MsmqTransport()
                .DontUseTransactions()
                .UnicastBus()
-               .SendOnly();  // This line turns on the basic diagnostics and request tracing
-            
+               .SendOnly();  // This line turns on the basic diagnostics and request tracing                        
+
+            new BootstrapRaven().Init();
+
             IncludeDiagnostics(true);
 
             Applies
@@ -38,7 +42,7 @@ namespace Hygia.API
             // in this assembly are assumed to be action methods
             Actions.IncludeClassesSuffixedWithController();
 
-           
+            ApplyConvention<PersistenceConvention>();
 
             // Policies
             Routes
@@ -49,108 +53,89 @@ namespace Hygia.API
             // Match views to action methods by matching
             // on model type, view name, and namespace
             Views.TryToAttachWithDefaultConventions();
-
-          
-         
         }
+
     }
 
-    public class RavenDatabaseSelectBehavior : BasicBehavior
+    class BootstrapRaven
     {
-        private readonly IFubuRequest _fubuRequest;
-        private IDocumentStore Store { get; set; }
-
-        public RavenDatabaseSelectBehavior(IFubuRequest fubuRequest) : base(PartialBehavior.Executes)
+        public void Init()
         {
-            _fubuRequest = fubuRequest;
+            environmentIdToDatabaseLookup.Add(Guid.Parse("327951bf-bae4-46a4-93a0-71f61dfbe801"), "Hygia.Acme");
+            var store = new DocumentStore
+                            {
+                                Url = "http://localhost:8080",
+                                DefaultDatabase = Configure.EndpointName
+                            };
+
+            store.Initialize();
+
+            ObjectFactory.Configure(c =>
+                                        {
+                                            c.ForSingletonOf<IDocumentStore>()
+                                                .Use(store);
+                                            c.For<IDocumentSession>()
+                                                .HybridHttpOrThreadLocalScoped()
+                                                .Use(OpenSession);
+
+                                            PluginCache.AddFilledType(typeof (IDocumentSession));
+                                        });
         }
 
-        protected override DoNext performInvoke()
+        static IDocumentSession OpenSession(IContext ctx)
         {
-            var p = _fubuRequest.Get<ModelBase>();
+            var fubuRequest = ctx.GetInstance<IFubuRequest>();
 
-            Store.OpenSession(p.Environment);
-            return base.performInvoke();
+            string database = null;
+
+            var modelBase = fubuRequest.Get<ModelBase>();
+
+            if (modelBase != null)
+            {
+                try
+                {
+                    database = environmentIdToDatabaseLookup[Guid.Parse(modelBase.Environment)];
+                }
+                catch (Exception)
+                {
+                    throw new Exception("No environment exists for:" + modelBase.Environment);
+                }                
+            }
+
+            var s = ctx.GetInstance<IDocumentStore>();
+
+            if (string.IsNullOrEmpty(database))
+                return s.OpenSession();
+
+            return s.OpenSession(database);
+        }
+
+        static readonly IDictionary<Guid, string> environmentIdToDatabaseLookup = new Dictionary<Guid, string>();
+    }
+
+    public class PersistenceConvention : IConfigurationAction
+    {
+        public void Configure(BehaviorGraph graph)
+        {
+            graph.Actions()
+                .Each(actionCall => actionCall.AddBefore(Wrapper.For<PersistenceBehavior>()));
         }
     }
 
-    //public class UnitOfWorkBehavior : IActionBehavior
-    //{
-    //    private readonly IDocumentStore _store;
-    //    private readonly IFubuRequest _fubuRequest;
-    //    private readonly IDocumentSession _session = null;
-    //    public IActionBehavior InnerBehavior { get; set; }
+    public class PersistenceBehavior : BasicBehavior
+    {
+        private readonly IDocumentSession _session;
 
-    //    public UnitOfWorkBehavior(IDocumentStore store, IFubuRequest fubuRequest)
-    //    {
-    //        _store = store;
-    //        _fubuRequest = fubuRequest;
-    //    }
+        public PersistenceBehavior(IDocumentSession session)
+            : base(PartialBehavior.Ignored)
+        {
+            _session = session;
+        }
 
-    //    public void Invoke()
-    //    {
-    //        _fubuRequest.Set(
-    //            new Lazy<IDocumentSession>(
-    //                () => _session ??
-    //                (_fubuRequest.Has<ModelBase>()
-    //                    ? _store.OpenSession(_fubuRequest.Get<ModelBase>().Environment)
-    //                    : _store.OpenSession())));
-
-    //        InnerBehavior.Invoke();
-
-    //        _session.SaveChanges();
-    //    }
-
-    //    public void InvokePartial()
-    //    {
-    //        InnerBehavior.InvokePartial();
-    //    }
-    //}
-
-    //class BootstrapRaven : IWantCustomInitialization
-    //{
-    //    public void Init()
-    //    {
-    //        environmentIdToDatabaseLookup.Add(Guid.Parse("327951bf-bae4-46a4-93a0-71f61dfbe801"), "Hygia.Acme");
-    //        var store = new DocumentStore
-    //        {
-    //            Url = "http://localhost:8080",
-    //            DefaultDatabase = Configure.EndpointName
-
-    //        };
-
-    //        store.Initialize();
-
-    //        ObjectFactory.Configure(c =>
-    //        {
-    //            c.ForSingletonOf<IDocumentStore>()
-    //                .Use(store);
-    //            c.For<IDocumentSession>()
-    //                .Use(OpenSession);
-
-    //            PluginCache.AddFilledType(typeof(IDocumentSession));
-
-    //            c.For<IManageUnitsOfWork>()
-    //                .Use<RavenUnitOfWork>();
-    //        });
-    //    }
-
-    //    static IDocumentSession OpenSession(IFubuRequest request)
-    //    {
-    //        var bus = ctx.GetInstance<IBus>();
-    //        string database = null;
-
-    //        if (bus.CurrentMessageContext != null && bus.CurrentMessageContext.Headers.ContainsKey("EnvironmentId"))
-    //            database = environmentIdToDatabaseLookup[Guid.Parse(bus.CurrentMessageContext.Headers["EnvironmentId"])];
-
-    //        var s = ctx.GetInstance<IDocumentStore>();
-
-    //        if (string.IsNullOrEmpty(database))
-    //            return s.OpenSession();
-
-    //        return s.OpenSession(database);
-    //    }
-
-    //    static readonly IDictionary<Guid, string> environmentIdToDatabaseLookup = new Dictionary<Guid, string>();
-    //}
+        protected override void afterInsideBehavior()
+        {
+            _session.SaveChanges();
+            _session.Dispose();
+        }
+    }
 }
