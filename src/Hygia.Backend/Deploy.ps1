d0ae6a304396d44cc85043e5ed80ca2fde745797
andreasohlund <andreasohlund2@gmail.com>
2012-02-13 07:53:38 +0100

    Adding deploy scripts for the backend

---
+++ src/Hygia.Backend/Deploy.ps1
@@ -0,0 +1,8 @@
+﻿$ServiceName = "Hygia.Backend"
+
+$service = Get-Service $ServiceName -ErrorAction SilentlyContinue
+if (!$service)
+{
+    Write-Host "The service will be installed"
+    & ".\NServiceBus.Host.exe" /install NServiceBus.Production | Write-Host
+}
