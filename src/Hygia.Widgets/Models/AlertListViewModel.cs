1a7319729826129cbbc4a6491536b956e7850055
Daniel Kjellqvist <daniel@kjellqvist.nu>
2012-02-24 08:32:22 +0100

    Changed alertlist

--- src/Hygia.Widgets/Models/AlertListViewModel.cs
+++ src/Hygia.Widgets/Models/AlertListViewModel.cs
@@ -8,9 +8,9 @@ namespace Hygia.Widgets.Models
     {
         public IList<Notification> Alerts = new List<Notification>
                                                        {
-                                                           new Notification{Title = "Första", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat"},
-                                                           new Notification{Title = "Andra", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat"},
-                                                           new Notification{Title = "Tredje", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat"},
+                                                           new Notification{Title = "1:a Database connection lost", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat", Description = "Message: Database connection lost\nReason: ProcessingFailed\nExceptionType: System.Exception\nSource: MyServer"},
+                                                           new Notification{Title = "2:a Database connection lost", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat", Description = "Message: Database connection lost\nReason: ProcessingFailed\nExceptionType: System.Exception\nSource: MyServer"},
+                                                           new Notification{Title = "3:e Database connection lost", NotificationDate = DateTime.Now, Summary = "Ett allvarligt fel har inträffat", Description = "Message: Database connection lost\nReason: ProcessingFailed\nExceptionType: System.Exception\nSource: MyServer"},
                                                        };
     }
 }
