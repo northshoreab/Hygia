6366fe30f81e2e59423cf92e5539e116c93d71ea
Daniel Kjellqvist <daniel@kjellqvist.nu>
2012-03-13 16:18:05 +0100

    Changed email template

--- src/Hygia.FaultManagement/FaultInfoProvider.cs
+++ src/Hygia.FaultManagement/FaultInfoProvider.cs
@@ -1,8 +1,24 @@
-﻿namespace Hygia.FaultManagement
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace Hygia.FaultManagement
 {
     using Domain;
     using Notifications.Provide;
     using Raven.Client;
+    
+    public static class XmlDocumentExtensions
+    {
+        public static string ToIndentedString(this XmlDocument doc)
+        {
+            var stringWriter = new StringWriter(new StringBuilder());
+            var xmlTextWriter = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented };
+            doc.Save(xmlTextWriter);
+            return stringWriter.ToString();
+        }
+    }
 
     public class FaultInfoProvider:IProvideFaultInformation
     {
@@ -11,13 +27,24 @@
         {
             var fault = Session.Load<Fault>(parameters.FaultEnvelopeId);
 
+            string prettyPrintedBody = fault.Body;
+
+            try
+            {
+                var bodyXmlDoc = new XmlDocument();
+                bodyXmlDoc.LoadXml(fault.Body);
+                prettyPrintedBody = bodyXmlDoc.ToIndentedString();
+            }
+            catch { }
+
             return new
                        {
                            fault.TimeOfFailure,
                            fault.Exception.Message,
                            fault.Exception.Reason,
                            fault.Body,
-                           fault.Headers
+                           fault.Headers,
+                           PrettyPrintedBody = prettyPrintedBody
                        };
         }
     }
