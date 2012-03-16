ad9be474a53f6342896d88717e3eff5808c4c2dd
andreasohlund <andreasohlund2@gmail.com>
2012-03-16 08:30:46 +0100

    Adjusted the taglines and also filters out fault where a retry has been performed ok but we don't know the outcome yet

--- src/Hygia.FaultManagement.Api/FaultsController.cs
+++ src/Hygia.FaultManagement.Api/FaultsController.cs
@@ -27,7 +27,7 @@ namespace Hygia.FaultManagement.Api
                        }.ToOutputModels();
 			*/
             return Session.Query<Fault>()
-                .Where(f => f.Status != FaultStatus.Archived && f.Status != FaultStatus.Resolved)
+                .Where(f => f.Status != FaultStatus.Archived && f.Status != FaultStatus.Resolved && f.Status != FaultStatus.RetryPerformed)
                 .OrderByDescending(f=>f.TimeOfFailure)
                 .ToList()
                 .ToOutputModels();
