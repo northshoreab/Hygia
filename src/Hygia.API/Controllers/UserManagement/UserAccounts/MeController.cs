18b4e04b3b29552b7a9ee365d0cd09125a150883
Daniel Kjellqvist <daniel@kjellqvist.nu>
2012-06-15 13:01:31 +0200

    Added controller to get current authenticated user info like accesstoken and name

---
+++ src/Hygia.API/Controllers/UserManagement/UserAccounts/MeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AttributeRouting;
+using AttributeRouting.Web.Http;
+using Hygia.API.Authentication;
+using Hygia.API.Models.UserManagement.UserAccounts;
+using Microsoft.IdentityModel.Claims;
+
+namespace Hygia.API.Controllers.UserManagement.UserAccounts
+{
+    [DefaultHttpRouteConvention]
+    [RoutePrefix("api/usermanagement/me")]
+    public class MeController : ApiController
+    {
+        public Me Get()
+        {
+            var user = User as IClaimsIdentity;
+
+            if(user == null)
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+
+            return new Me
+                       {
+                           AccessToken = user.GetClaimValue(Constants.ClaimTypes.GithubAccessToken),
+                           Name = user.Name
+                       };
+        }
+    }
+}
---
+++ src/Hygia.API/Models/UserManagement/UserAccounts/Me.cs
@@ -0,0 +1,8 @@
+namespace Hygia.API.Models.UserManagement.UserAccounts
+{
+    public class Me
+    {
+        public string Name { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
