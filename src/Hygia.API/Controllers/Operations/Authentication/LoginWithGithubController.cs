839702c374a7bdfd06e0a1910e92c0db6c228543
Daniel Kjellqvist <daniel@kjellqvist.nu>
2012-10-03 16:00:34 +0200

    Added controller for login

--- src/Hygia.API/Controllers/Operations/Authentication/LoginWithGithubController.cs
+++ src/Hygia.API/Controllers/Operations/Authentication/LoginWithGithubController.cs
@@ -2,14 +2,75 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
+using System.Web.Security;
 using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Threading;
 using AttributeRouting;
 using AttributeRouting.Web.Http;
 using DotNetOpenAuth.AspNet;
 using Microsoft.Web.WebPages.OAuth;
+using System;
 
 namespace Hygia.API.Controllers.Operations.Authentication
 {
+    [DefaultHttpRouteConvention]
+    [RoutePrefix("api")]
+    [Authorize]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        [POST("login")]
+        //[ValidateAntiForgeryToken]
+        public void Login(string provider, string returnUrl)
+        {
+            OAuthWebSecurity.RequestAuthentication(provider, "http://localhost/api/login/authenticationcallback");
+        }
+
+        [AllowAnonymous]
+        [POST("authenticationcallback")]
+        public HttpResponseMessage AuthenticationCallback()
+        {
+            var queryString = ControllerContext.Request.RequestUri.ParseQueryString();
+            
+            if (queryString.AllKeys.All(x => x != "__sid__") && ControllerContext.Request.RequestUri != null && queryString.AllKeys.Any(x => x == "state"))
+            {
+                var response = ControllerContext.Request.CreateResponse(HttpStatusCode.RedirectKeepVerb);
+                response.Headers.Location = new Uri(ControllerContext.Request.RequestUri.AbsoluteUri + "&__sid__=" + queryString.Get("state"));
+                return response;
+            }
+
+            var result = OAuthWebSecurity.VerifyAuthentication();
+
+            if (result.IsSuccessful)
+            {
+                // name of the provider we just used
+                var provider = result.Provider;
+                // provider's unique ID for the user
+                var uniqueUserID = result.ProviderUserId;
+                // since we might use multiple identity providers, then 
+                // our app uniquely identifies the user by combination of 
+                // provider name and provider user id
+                var uniqueID = provider + "/" + uniqueUserID;
+
+                // we then log the user into our application
+                // we could have done a database lookup for a 
+                // more user-friendly username for our app
+                FormsAuthentication.SetAuthCookie(uniqueID, false);
+
+                // dictionary of values from identity provider
+                var userDataFromProvider = result.ExtraData;
+                var email = userDataFromProvider["email"];
+                var gender = userDataFromProvider["gender"];
+
+                return new HttpResponseMessage(HttpStatusCode.Accepted);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
+    }
+
     [DefaultHttpRouteConvention]
     [RoutePrefix("api/login/withgithub")]
     [Authorize]
--- src/Hygia.Operations/MessageConventions.cs
+++ src/Hygia.Operations/MessageConventions.cs
@@ -7,7 +7,7 @@
     {
          public static Configure HygiaMessageConventions(this Configure config)
          {
-             return config.DefiningMessagesAs(t => t.Namespace != null && (t.Namespace.EndsWith(".Messages") || typeof(ITimeoutState).IsAssignableFrom(t)))
+             return config.DefiningMessagesAs(t => t.Namespace != null && ((t.Namespace.EndsWith(".Messages") && t.Namespace.StartsWith("Hygia")) || typeof(ITimeoutState).IsAssignableFrom(t)))
                  .DefiningCommandsAs(t => t.Namespace != null && !t.Namespace.StartsWith("Hygia.API.Controllers") && t.Namespace.EndsWith(".Commands"))
                  .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Events"));
          }
